722a53a9febdf6a0edbe449f95e380eddc2b41ce
dnyaneshwarrathod245 <rdnyaneshwar947@gmail.com>
2026-07-09 01:59:57 +0530

    Added July_07Day33 Netflix clone

---
+++ 01-July_Day31/React js/July_07Day33/Netflix clone/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import MovieRow from "./components/MovieRow";
+
+function App(){
+  return(
+    <>
+    <Navbar/>
+    <Hero/>
+    <MovieRow/>
+    </>
+
+  );
+}
+export default App;
